e52b64bb644593253fd6914c78de763e47f36f97
Hyugasi <isaiah.gordon@gmail.com>
2020-05-18 07:28:44 -0500

    tried project euler

---
+++ project_euler_multiples_3_5.js
@@ -0,0 +1,16 @@
+/*
+FUNC multiples takes a number
+    
+ENDFUNC
+*/
+
+function multiples(num) {
+    let sum = 0
+    if(num < 10000){
+        if(num % 5===0 & num % 3===0){
+            return "hello"
+        }
+    }
+}
+
+console.log(multiples(1000))
